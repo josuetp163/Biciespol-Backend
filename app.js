90892e27370e4a7c5a8db053547bfb07e37fdc5a
josuetp163 <jotopozo@espol.edu.ec>
2021-01-08 02:38:30 -0500

    Connexion

--- app.js
+++ app.js
@@ -6,8 +6,22 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var mailRouter = require('./routes/mail');
 
 var app = express();
+
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+var corsOptions = {origin: "http://localhost:4200"};
+app.use(cors(corsOptions)); 
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json()); 
+
+// parse requests of content-type - application/x-www-form-urlencoded
+//app.use(bodyParser.urlencoded({​​​​​​​​extended: true}​​​​​​​​));
+app.use(bodyParser.urlencoded({extended:true}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +35,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/mail', mailRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/mail.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+var nodemailer = require("nodemailer")
+
+
+/* GET home page. */
+router.post('/', function(req, res, next) {
+    console.log(req.body)
+    res.render('index', { title: 'Express' });
+    mail(req); 
+});
+
+async function mail(req){
+  // Generate test SMTP service account from ethereal.email
+  // Only needed if you don't have a real mail account for testing
+  let testAccount = await nodemailer.createTestAccount();
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    //host: "smtp.ethereal.email",
+    //port: 587,
+    //secure: false, // true for 465, false for other ports
+    auth: {
+      //user: "emmie.considine21@ethereal.email", // generated ethereal user
+      //pass: "txBsB4sdXstd83frSB", // generated ethereal password
+        user: "josuetp987@gmail.com",
+        pass: "fragola1",
+    },
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: req.body.email, // sender address
+    to: "josuetp987@gmail.com", // list of receivers
+    subject: req.body.nombre, // Subject line
+    text: req.body.email +"\n"+ req.body.mensaje, // plain text body
+    //html: "<b>Hello world?</b>", // html body
+  });
+}
+
+module.exports = router;
